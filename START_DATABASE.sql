8e8d991eeb05e7a77c571a9623f230512e8e438f
jgonzalezef <jesusgonzalez@MacBook-Pro-de-Envia.local>
2023-11-18 17:24:18 -0600

    start database

---
+++ START_DATABASE.sql
@@ -0,0 +1,19 @@
+
+CREATE TABLE `orders` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `total` double NOT NULL DEFAULT 0,
+  `status` varchar(100) NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `order_items` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` bigint(20) unsigned NOT NULL,
+  `order_id` bigint(20) unsigned NOT NULL,
+  `amount` bigint(20) unsigned NOT NULL,
+  `price` double NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
