56e2ac80fbd07dbe544367218ccc298d3f39dcb2
jgonzalezef <jesusgonzalez@MacBook-Pro-de-Envia.local>
2023-11-18 17:32:06 -0600

    agregar morgan para verificar las peticiones del api gateway y de servicios

--- gateway/src/app.ts
+++ gateway/src/app.ts
@@ -1,11 +1,14 @@
 import express, {Application} from 'express';
 import proxy from 'express-http-proxy';
+import morgan from 'morgan';
 
 import dotenv from 'dotenv';
 import { Signale } from "signale";
 
 const app:Application = express();
 const signale = new Signale();
+
+app.use(morgan('dev'));
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
--- order-services/src/app.ts
+++ order-services/src/app.ts
@@ -1,10 +1,14 @@
 import express, {Application} from 'express';
 import dotenv from 'dotenv';
 import { Signale } from "signale";
+import morgan from 'morgan';
+
 import { orderRouter } from './order/infraestructure/routes/orderRoutes';
 
 const app:Application = express();
 const signale = new Signale();
+
+app.use(morgan('dev'));
 
 dotenv.config();
 const PORT = process.env.PORT || 3001;
